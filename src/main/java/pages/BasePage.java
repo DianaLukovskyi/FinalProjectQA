6e318802fa7378b53c89ec9abc33c3863d3b9eae
AIT TR Student <17popovaas@gmail.com>
2024-05-31 10:44:03 +0200

    init

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,43 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BasePage {
+    WebDriver driver;
+    JavascriptExecutor js;
+    static Logger logger = LoggerFactory.getLogger(BasePage.class);
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        // with chain and @FindBy
+        PageFactory.initElements(driver, this);
+        js = (JavascriptExecutor) driver;
+    }
+
+    public void clickBase(WebElement element) {
+        element.click();
+    }
+
+    public void typeText(WebElement element, String text) {
+        element.click();
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getTextBase(WebElement element) {
+        return element.getText().trim();
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
